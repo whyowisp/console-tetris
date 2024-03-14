7ddda69b6ac27d067256ebdba5a43f907675a559
whyowisp <95976862+whyowisp@users.noreply.github.com>
2024-03-14 14:58:22 +0200

    user input and game loop in separate threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::thread;
+
 mod game_frame;
 mod user_input;
 
 fn main() {
-    let _result = game_frame::draw();
-    user_input::listen();
+    let input_loop = thread::spawn(|| {
+        let _result = user_input::listen();
+    });
+    let game_loop = thread::spawn(|| {
+        let _result = game_frame::draw();
+    });
+
+    input_loop.join().expect("Input loop panicked");
+    game_loop.join().expect("game_loop panicked");
 }
