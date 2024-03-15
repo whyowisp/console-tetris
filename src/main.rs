d887776bec9c47bc809c584dcb40261a185b1d41
whyowisp <95976862+whyowisp@users.noreply.github.com>
2024-03-15 17:49:43 +0200

    back to use of termios with user input

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,14 @@ use std::io::Error;
 use std::thread;
 
 //a change
-fn read_character() -> io::Result<char> {
-    // Read a single character from stdin
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-
-    // Extract the first character (if any)
-    if let Some(c) = input.chars().next() {
-        Ok(c)
-    } else {
-        Err(Error::new(
-            io::ErrorKind::InvalidInput,
-            "No character found",
-        ))
-    }
-}
 
 fn main() -> io::Result<()> {
     loop {
-        match read_character() {
-            Ok(c) => println!("you typed: {}", c),
+        match user_input::read_character() {
+            Ok(c) => {
+                println!("you typed: {}", c)
+            }
+
             Err(e) => eprintln!("Error: {}", e),
         }
     }
--- src/user_input.rs
+++ src/user_input.rs
@@ -1,14 +1,45 @@
-use crossterm::event::{self, Event, KeyCode, KeyEvent};
-use std::io::{self, Result};
+use crossterm::event::{self, KeyCode, KeyEvent, KeyModifiers};
+use std::io;
+//use std::io::Error;
 
-pub fn listen() -> io::Result<char> {
-    loop {
-        if let Event::Key(KeyEvent {
-            code: KeyCode::Char(c),
-            ..
-        }) = event::read()?
-        {
-            return Ok(c);
+pub fn read_character() -> io::Result<char> {
+    if let event::Event::Key(KeyEvent {
+        code, modifiers, ..
+    }) = event::read()?
+    {
+        // Check if it's a regular key (no modifiers)
+        if modifiers == KeyModifiers::NONE {
+            match code {
+                KeyCode::Char(c) => return Ok(c),
+                _ => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "No character entered",
+                    ))
+                }
+            }
         }
     }
+
+    Err(io::Error::new(
+        io::ErrorKind::InvalidInput,
+        "No character entered",
+    ))
 }
+
+/*
+pub fn read_character() -> io::Result<char> {
+    // Read a single character from stdin
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    // Extract the first character (if any)
+    if let Some(c) = input.chars().next() {
+        Ok(c)
+    } else {
+        Err(Error::new(
+            io::ErrorKind::InvalidInput,
+            "No character found",
+        ))
+    }
+}*/
